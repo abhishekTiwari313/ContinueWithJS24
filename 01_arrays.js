0b1be57825b97e097be41fb6060a2a9324fb6c38
Abhishek Tiwari <abhishek.at313@gmail.com>
2024-01-07 11:32:43 +0530

    all about the concept of array basics

---
+++ 01_arrays.js
@@ -0,0 +1,29 @@
+// array
+
+const myArr = [0,1,2,3,4,5,true,"hitesh"];
+
+const myHeroes = ["shaktiman", "naagraj"];
+
+const myArr2 = new Array (1,2,3,4)
+
+console.log(myArr[1]);
+// Array methods
+
+myArr.push(6)
+//console.log(myArr)
+
+myArr.pop()
+//console.log(myArr)
+
+myArr.unshift(9);
+//console.log(myArr)
+
+myArr.shift()
+//console.log(myArr)
+
+const newArr = myArr.join();
+
+console.log(myArr);
+console.log( newArr);
+
+
